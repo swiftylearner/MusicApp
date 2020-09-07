e2fe5bd627d78623a1fe1dda00281f19511e5a0e
guyvenson cadet <createWithCodes@gmail.com>
2020-09-07 11:33:57 -0400

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by MacBook on 9/7/20.
+//  Copyright © 2020 Apptive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
